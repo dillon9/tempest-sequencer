045a841d3e8ef5d5ece86fd9853281f92f4ea52a
dillon <eater1234@gmail.com>
2021-12-18 13:03:46 -0800

    first

---
+++ gen.py
@@ -0,0 +1,31 @@
+
+from random import randint
+import sys
+
+def makeSong():
+    c = 0
+    song = ""
+    a = "ABCDEFG"
+    b = "12345678"
+    d = "#b"
+
+    while c < randint(int(sys.argv[1]), int(sys.argv[2])):
+        song += a[randint(0, len(a)-1)]
+        if randint(0, 1):
+            song += d[randint(0, 1)]
+        song += b[randint(0, len(b)-1)]
+        if randint(0, 10) >= 7:
+            song += " - " if randint(0, 1) else " . "
+        else:
+            song += " "
+        c += 1
+
+    return song.strip()
+
+
+c = 0
+while c < int(sys.argv[3]):
+    fob = open(str(c) + ".txt", "w")
+    fob.write(makeSong())
+    fob.close()
+    c += 1
